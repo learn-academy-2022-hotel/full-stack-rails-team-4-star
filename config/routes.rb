b8a838f56e70b420e4d5cff2271777762430ec81
nvencer98 <vencer98@gmail.com>
2023-01-19 11:32:12 -0800

    delete complete" "

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ get '/blogs' => 'blog#index', as: 'blogs'
 get 'blogs/new' => 'blog#new', as: 'new'
 get '/blogs/:id' => 'blog#show', as: 'blog'
 post 'blogs' => 'blog#create'
+delete 'blogs/:id' => 'blog#destroy', as: 'delete'
 
 end
