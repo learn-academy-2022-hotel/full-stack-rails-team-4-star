7654202c1752a57fc2d9869f4fd4240a5a9e3376
nvencer98 <vencer98@gmail.com>
2023-01-19 11:03:07 -0800

    new completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'blog#index'
 get '/blogs' => 'blog#index', as: 'blogs'
+get 'blogs/new' => 'blog#new', as: 'new'
 get '/blogs/:id' => 'blog#show', as: 'blog'
+
 end
